dcedf3d8534d851c1bedf23183959b67dd8b9ee5
Tavish Gupta <tavishgupta7@gmail.com>
2025-03-11 01:19:39 -0400

    Data Ingestion

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from src.studentperformance.logger import logging
 from src.studentperformance.exception import CustomException
+from src.studentperformance.components.data_ingestion import DataIngestion
+from src.studentperformance.components.data_ingestion import DataIngestionConfig
 import sys
 
 
@@ -8,7 +10,11 @@ if __name__ == "__main__":
 
 
     try:
-        a=1/0
+        #data_ingestion_config = DataIngestionConfig()
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+
+
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e, sys)
